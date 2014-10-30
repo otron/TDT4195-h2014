32b5d81318ea67261774054c9d8a5fab25dcceb7
Odd <oddweb42+gith@gmail.com>
2014-10-30 11:31:03 +0100

    ilab-01 finished the practice exercises.
    Guess I know matlab/octave now!
    Don't know why all the kids in ITGK are having such a hard time
    with it. All the answers are available on google.

--- image-lab/01/code/IPexercise01.m
+++ image-lab/01/code/IPexercise01.m
@@ -94,18 +94,28 @@ img_gs = rgb2gray(img);
 % Task 3.3: Convert image to floating point (double) representation
 
 img_fp = im2double(img);
-imshow(img_fp)
-wait(50000)
+imwrite(img_fp, "fprep.png");
+imwrite(img_gs, "gs.png");
 
 % Task 3.4: Show image on screen (remember to put figure; before this and all other commands that displays something on screen to insure that all images are shown)
+imshow(img_fp)
 
 % Task 3.5: For all pixels (elements in the image matrix) with values above 0.7 set the pixel value to 1.0 (recall task 1.10)
+img_fx = img;
+img_fx(img_fx > 255*0.7) = 255;
 
 % Task 3.6: Show image on screen
+imshow(img_fx)
 
 % Task 3.7: Compute the maximum and minimum gray-value of the image
+gray_max = max(img_gs(:))
+gray_min = min(img_gs(:))
 
 % Task 3.8: Create and show a histogram of the image
+imhist(img);
 
 % Task 3.9: Save image to disk and call it result.png
+export(imhist(img), "result.png");
 
+
+pause() % sending 'k' to the terminal it's running in kills it
