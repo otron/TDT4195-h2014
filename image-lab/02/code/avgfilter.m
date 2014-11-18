a3d3303760276f116df1aa68e0fcea29703f8074
Odd <oddweb42+gith@gmail.com>
2014-11-18 17:35:40 +0100

    ilab-02 forgot to multiply by 1/n*m

--- image-lab/02/code/avgfilter.m
+++ image-lab/02/code/avgfilter.m
@@ -5,7 +5,7 @@ function res = avgfilter(img, n, m)
     res = img(a+1:height-a, b+1:width-b);
     for i = a+1:height-a
         for j = b+1:width-b
-            res(i-a, j-b) = avg(neighborhood(img, i, j, n, m));
+            res(i-a, j-b) = (1/(n*m))*avg(neighborhood(img, i, j, n, m));
         end
     end
 end
