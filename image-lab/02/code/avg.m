9b3279557237f8751379570d57634a27cd8b85d5
Odd <oddweb42+gith@gmail.com>
2014-11-19 09:14:18 +0100

    ilab-02 wrote some tests for avg.m

--- image-lab/02/code/avg.m
+++ image-lab/02/code/avg.m
@@ -4,3 +4,8 @@ function res = avg(T)
         res = mean(res);
     end
 end
+
+%!assert (avg(ones(4)) == 1)
+%!assert (avg(ones(3,3,3,3)) == 1)
+%!assert (avg(zeros(3,3)) == 0)
+%!assert (length(avg(rand(randi(1000)))) == 1)
