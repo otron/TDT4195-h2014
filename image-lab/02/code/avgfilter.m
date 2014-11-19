15b3d7d0c5a31723b13d76b84e5b4688ba1dc09c
Odd <oddweb42+gith@gmail.com>
2014-11-19 09:00:09 +0100

    ilab-02 made a+1 and b+1 into variables

--- image-lab/02/code/avgfilter.m
+++ image-lab/02/code/avgfilter.m
@@ -2,9 +2,11 @@ function res = avgfilter(img, n, m)
     [height, width] = size(img);
     a = (n-1)/2;
     b = (m-1)/2;
-    res = img(a+1:height-a, b+1:width-b);
-    for i = a+1:height-a
-        for j = b+1:width-b
+    newOx = a+1;
+    newOy = b+1;
+    res = img(newOx:height-a, newOy:width-b);
+    for i = newOx:height-a
+        for j = newOy:width-b
             res(i-a, j-b) = (1/(n*m))*avg(neighborhood(img, i, j, n, m));
         end
     end
