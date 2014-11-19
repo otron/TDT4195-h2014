207f20d7c1fac7339f0cdb18b844eb6b19148940
Odd <oddweb42+gith@gmail.com>
2014-11-19 10:49:28 +0100

    ilab-02 updated makefile w/ renames, updated script to generate noisy images

--- image-lab/02/code/2.1.m
+++ image-lab/02/code/2.1.m
@@ -1,11 +1,26 @@
-img1 = imread('img/withnoise.png');
-img2 = imread('img/withgaussian.png');
+% generate noisy images
+src = double(imread('img/cameraman.png'));
+src = double(src)/255.0;
+
+% s&p
+density = 0.25;
+imwrite(imnoise(src, 'salt & pepper', density),
+    '2_out/2.1_sp.png');
+
+% gaus
+avg = 0.0; stdd = 0.1;
+imwrite(imnoise(src, 'gaussian', avg, stdd),
+    '2_out/2.1_gaus.png');
+
+
+img1 = imread('2_out/2.1_sp.png');
+img2 = imread('2_out/2.1_gaus.png');
 
 vals = [3, 5, 9];
 
 for i = 1:length(vals)
-    imwrite(avgfilter(img1, vals(i), vals(i)), 
+    imwrite(avgfilter(img1, vals(i), vals(i)),
         strcat('2_out/2.1_sp_', num2str(vals(i)), 'x', num2str(vals(i)), '.png'));
-    imwrite(avgfilter(img2, vals(i), vals(i)), 
+    imwrite(avgfilter(img2, vals(i), vals(i)),
         strcat('2_out/2.1_gaus_', num2str(vals(i)), 'x', num2str(vals(i)), '.png'));
 end
